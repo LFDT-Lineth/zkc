fe0e8a86e03f13a26eb28e866b5be1b5c4ae0d54
amkCha <29160563+amkCha@users.noreply.github.com>
2026-05-26 11:24:02 +0200

    add 2 test cases to fix é

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -320,6 +320,14 @@ func Test_ZkcUnit_FixedArray_21(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/fixed_array_21", util.DEFAULT_CONFIG)
 }
 
+func Test_ZkcUnit_FixedArray_22(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/fixed_array_22", util.DEFAULT_CONFIG)
+}
+
+func Test_ZkcUnit_FixedArray_23(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/fixed_array_23", util.DEFAULT_CONFIG)
+}
+
 // ===================================================================
 // Type Tests
 // ===================================================================
