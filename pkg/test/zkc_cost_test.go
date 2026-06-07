38c3871422e672fd14d6107f51db3b5da4ec92b5
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-06-07 21:27:17 +0200

    lint
    Signed-off-by: Lorenzo Gentile <lorenzo.gentile@consensys.net>

--- pkg/test/zkc_cost_test.go
+++ pkg/test/zkc_cost_test.go
@@ -30,18 +30,21 @@ func Test_ZkcUnit_Cost_01(t *testing.T) {
 
 func Test_ZkcUnit_Cost_01_ReportIncludesLoweredLabels(t *testing.T) {
 	path := filepath.Join(util.TestDir, "zkc/unit/cost_01.zkc")
+
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	src := source.NewSourceFile(path, bytes)
+
 	program, _, errors := compiler.Compile(field.KOALABEAR_16, *src)
 	if len(errors) != 0 {
 		t.Fatalf("unexpected compile errors: %v", errors)
 	}
 
 	report := codegen.NewCostReport()
+
 	_, errors = program.Compile(codegen.DEFAULT_CONFIG.Field(field.KOALABEAR_16).CostReport(report))
 	if len(errors) != 0 {
 		t.Fatalf("unexpected codegen errors: %v", errors)
