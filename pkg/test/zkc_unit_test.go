62365a3126ce0492cf3d1a811c3abfd0e3868128
DavePearce <dave01001110@gmail.com>
2026-06-12 16:03:01 +1200

    feat: update tests
    Signed-off-by: DavePearce <dave01001110@gmail.com>

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -222,6 +222,10 @@ func Test_ZkcUnit_Basic_45(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/basic_45", DEFAULT_UNIT_CONFIG.Constraints(false))
 }
 
+func Test_ZkcUnit_Basic_46(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/basic_45", DEFAULT_UNIT_CONFIG.Constraints(false))
+}
+
 // ===================================================================
 // If-Else-If Tests
 // ===================================================================
