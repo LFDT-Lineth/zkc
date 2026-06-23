827b7e426a468cbb1ec8be88afb77de798f4bc6c
F Bojarski <ceciestunepoubelle@protonmail.ch>
2026-06-23 17:08:50 +0530

    test: add while test
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -498,6 +498,11 @@ func Test_ZkcUnit_While_03(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/while_03", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
 }
 
+// x = f(x) in a while loop
+func Test_ZkcUnit_While_04(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/while_04", DEFAULT_UNIT_CONFIG.Constraints(true))
+}
+
 func Test_ZkcUnit_For_01(t *testing.T) {
 	// TODO: bitwise destruct
 	checkZkcUnit(t, "zkc/unit/for_01", DEFAULT_UNIT_CONFIG)
