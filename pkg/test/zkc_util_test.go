2a4b8f12ac20f9cd67c232132727f1629927efc5
David Pearce <david.pearce@consensys.net>
2026-07-02 01:12:10 +1200

    feat: port assembly util tests (#1948)
    This ports the (now defunct) zkasm tests over to Zkc, along with their
    converted inputs.  In addition, for many tests this also adds a "native
    zkc" implementation which utilises ZkC primitives (e.g. bitwise
    operations), and tests that implementation as well.
    
    Signed-off-by: DavePearce <dave01001110@gmail.com>

---
+++ pkg/test/zkc_util_test.go
@@ -0,0 +1,104 @@
+// Copyright Consensys Software Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+package test
+
+import (
+	"testing"
+
+	"github.com/LFDT-Lineth/zkc/pkg/test/util"
+	"github.com/LFDT-Lineth/zkc/pkg/util/field"
+)
+
+func Test_ZkcUtil_Byte(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/byte", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_BitRor64(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/bit_ror64", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_BitSar(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/bit_sar", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_BitShr(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/bit_shr", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_BitShl(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/bit_shl", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_ByteCounting(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/byte_counting", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_ByteSize(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/byte_size", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_FillBytes(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/fill_bytes", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_FirstByte(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/first_byte", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_G1G2(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/g1g2", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Log2(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/log2", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Log256(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/log256", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Max2(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/max2", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Max3(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/max3", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Min(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/min", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Padding(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/padding", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_SetByte(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/set_byte", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_Signextend(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/signextend", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+func Test_ZkcUtil_SwitchEndian(t *testing.T) {
+	checkZkcUtil(t, "zkc/util/switch_endian", DEFAULT_UNIT_CONFIG.Fields(field.BLS12_377))
+}
+
+// ===================================================================
+// Test Helpers
+// ===================================================================
+
+func checkZkcUtil(t *testing.T, test string, config util.Config) {
+	util.CheckValid(t, test, "zkc", config)
+}
